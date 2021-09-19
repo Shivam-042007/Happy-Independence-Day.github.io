ddd3dbecab1d44e36f6fb40f4d6e0289cba71691
Shivam-042007 <85301544+Shivam-042007@users.noreply.github.com>
2021-09-19 10:46:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function hoist(){
+    document.getElementById("myAudio").play();
+    document.getElementById("flag").style.display="block";
+    document.getElementById("button").display="none";
+    document.getElementById("wish").display="block";
+  }
